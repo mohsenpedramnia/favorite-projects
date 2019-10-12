2014ba2ddb3fe26b09995d7b46c3769f2fe2a09e
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2019-10-12 17:50:14 +0330

    adding Content and Sidebar to App

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
 export const App = () => {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   );
 };
---
+++ todos/src/components/layout/Sidebar.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Sidebar = () => <p>I am a sidebar!</p>;
